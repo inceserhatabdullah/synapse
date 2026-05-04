690cc04aace0ef34bd5c232a37845c88a9f732f4
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-05-04 15:51:12 +0300

    migration

---
+++ Synapse.Infrastructure/Migrations/20260504084502_AddUpdateDateAtToModels.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Synapse.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdateDateAtToModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItems",
+                table: "OrderItems");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItems",
+                newName: "OrderItem");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItem",
+                newName: "IX_OrderItem_OrderId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserSessions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Orders_OrderId",
+                table: "OrderItem",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Orders_OrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderItem",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserSessions");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "OrderItem",
+                newName: "OrderItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItems",
+                newName: "IX_OrderItems_OrderId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderItems",
+                table: "OrderItems",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItems_Orders_OrderId",
+                table: "OrderItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
